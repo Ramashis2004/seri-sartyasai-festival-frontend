b2525a4a9eb09fee5659c3dc021c60f81e64f2ce
Ramashis2004 <dasramashsiskalyan@gamil.com>
2025-12-08 13:32:04 +0530

    If tab closed then logout

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: "http://localhost:4000/api",
 });
 
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
+  const token = sessionStorage.getItem("token");  // <-- changed
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
